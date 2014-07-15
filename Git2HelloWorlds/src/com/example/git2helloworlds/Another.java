e68b8a8eb7a110fab88c6202e00e57ee45c3c069
obrienj <obrien.joshua@live.com>
2014-07-15 10:34:10 -0400

    Update Another.java

--- Git2HelloWorlds/src/com/example/git2helloworlds/Another.java
+++ Git2HelloWorlds/src/com/example/git2helloworlds/Another.java
@@ -4,6 +4,8 @@ import android.app.Activity;
 
   public class Another extends Activity {
     
+    // Here is change from website.
+    
     // Edit made to this class from someone else.
     
     // Another, another change. But then I make it better because your change was bad.
